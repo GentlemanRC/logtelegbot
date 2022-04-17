5c2e39bbd392954daf7e0186c860c06379fdb063
GentlemanRC <91190804+GentlemanRC@users.noreply.github.com>
2022-04-17 23:04:52 +0500

    v 1.1
    Пофикшен баг с обновлением сообщений, из-за чего телеграмм может удалить сессию. Используйте .hiddenlog

--- main.py
+++ main.py
@@ -81,4 +81,71 @@ def log(_, msg):
 	except FloodWait as e:
 		time.sleep(e.x)
 
+@app.on_message(filters.command("hiddenlog", prefixes=".") & filters.me)
+def log(_, msg):
+	try:
+		if msg.command[1] != 'this':
+			orig_text = msg.command[1]
+			if msg.command[1] == 'seek':
+				for dialog in app.iter_dialogs():
+					if dialog.chat.title == msg.command[2]:
+						orig_text = dialog.chat.id
+						fromChannel = True
+						chattitle = dialog.chat.title
+		elif msg.command[1] == 'this':
+			fromChannel = False
+			orig_text = msg.chat.id
+		else:
+			fromChannel = False
+	except:
+		fromChannel = False
+		pass
+
+	msg.delete()
+	print("Начало сохранения логов.")
+	try:
+		for message in app.iter_history(orig_text):
+			log = open(f"logfile-{orig_text}.txt", "a", encoding="utf-8")
+			messageTime = datetime.datetime.fromtimestamp(message.date)
+			messagedater = datetime.datetime.fromtimestamp(message.date).date()
+			if 'messagePreviousDate' not in locals():
+				log.write(f"({messagedater})\n")
+				messagePreviousDate = messageTime = datetime.datetime.fromtimestamp(message.date).date()
+			elif messagePreviousDate != messageTime.date():
+				log.write(f"({messagedater})\n")
+				messagePreviousDate = messagedater
+				pass
+			if message.text is not None:
+					messagetimer = datetime.datetime.fromtimestamp(message.date).time()
+					if fromChannel is not True:
+						log.write(f"{message.from_user.first_name} ({messagetimer}): {message.text}\n")
+					else:
+						log.write(f"{chattitle} ({messagetimer}): {message.text}\n")
+					try:
+						if fromChannel is not True:
+							app.download_media(message)
+							log.write(f"{message.from_user.first_name} ({messagetimer}): <SENT MEDIA AND TEXT> {message.text}\n")
+						else:
+							app.download_media(message)
+							log.write(f"{chattitle} ({messagetimer}): <SENT MEDIA AND TEXT> {message.text}\n")
+					except:
+						pass
+			elif message.text is None:
+				try:
+					if fromChannel is not True:
+						app.download_media(message)
+						log.write(f"{message.from_user.first_name} ({messagetimer}): <SENT MEDIA>\n")
+					else:
+						app.download_media(message)
+						log.write(f"{chattitle} ({messagetimer}): <SENT MEDIA>\n")
+
+				except:
+					pass
+			log.close()
+			time.sleep(0.1)
+		print(f'Логи сохранены в файл: logfile-{orig_text}.txt')
+
+	except FloodWait as e:
+		time.sleep(e.x)
+
 app.run()
